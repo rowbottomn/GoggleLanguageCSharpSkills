a3e26bcd60156526cd5580a9312c95cabf226571
Nathan Rowbottom <rowbottomn@hdsb.ca>
2019-02-07 05:36:35 -0500

    Posted Program file
    Need to correct this

---
+++ TextToSpeech/RecordingFile/Program.cs
@@ -0,0 +1,48 @@
+using System;
+using Google.Cloud.TextToSpeech.V1;
+using System.IO;
+
+namespace TextToSpeechApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = TextToSpeechClient.Create();
+           // var response = client.ListVoices("fa"); 
+           // foreach (var voice in response.Voices)
+            //{    Console.WriteLine($"{voice.Name} ({voice.SsmlGender}); Language codes: {string.Join(", " ,voice.LanguageCodes)} ");
+           // }
+            
+            
+            //Voice input as text or SSML
+            var input = new SynthesisInput{
+                Text = "ahlaan wamarhabaan bikum fi madrasat krigh kilbirghr alththanawiat lilbawdakast"
+            };
+            
+            //Build the voice request
+            var voiceSelection = new VoiceSelectionParams
+            {
+                LanguageCode = "fr-FR",
+                SsmlGender = SsmlVoiceGender.Female
+            };
+
+            //lets specify the audio file type
+            var audioConfig = new AudioConfig
+            {
+                AudioEncoding = AudioEncoding.Mp3    
+            };
+            
+            //create the text-to-speech request
+            var response = client.SynthesizeSpeech(input, voiceSelection, audioConfig);
+            
+            //write the response to the output file
+            using (var output = File.Create("output_fr1.mp3"))
+            {
+                response.AudioContent.WriteTo(output);
+            }
+            Console.WriteLine("Audio content written to file \"output.mp3\"");
+            
+        }
+    }
+}
